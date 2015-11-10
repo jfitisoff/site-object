be8b9e9c84c07031e401a2a4071d377210cb0200
John Fitisoff <john@johns-air.gateway.pace.com>
2015-11-09 16:09:06 -0800

    updates

--- tests/unit_tests.rb
+++ tests/unit_tests.rb
@@ -3,9 +3,9 @@ describe "POC" do
     @b = Watir::Browser.new
   end
 
-  after(:all) do
-    @b.close
-  end
+  # after(:all) do
+  #   @b.close
+  # end
 
   it "runs a browser test" do
     @b.goto("https://www.google.com/")
