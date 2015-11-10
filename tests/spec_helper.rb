8e61aaeb23cbb7dd0c80927b78d4d8cc15e195b9
John Fitisoff <john@johns-air.gateway.pace.com>
2015-11-09 16:54:12 -0800

    trying to get the gihub example working

---
+++ tests/spec_helper.rb
@@ -0,0 +1,102 @@
+# spec_helper.rb
+# This example shows some site object code written for https://ruby-lang.org. The code here
+# implements enough functionality to write some code to test news posts on the site.
+require 'site-object'
+require 'watir-webdriver'
+require 'rspec'
+
+# The site object for ruby-lang.org.
+class RubyLangSite
+  attr_accessor :language
+  include SiteObject
+
+  def initialize(browser, language)
+    @language = language  # Set the attr_accessor defined for this class.
+    super base_url: "https://www.ruby-lang.org", browser: browser # Finish initialization.
+  end
+end
+
+# A page feature. This one models the header bar with links that runs across the top of all
+# of the site's pages. It can be added to a page object by calling the use_features method
+# when defining a page's class. When added to a page class, the initialized page has an
+# accessor method for it (see usage below.)
+class HeaderBar < PageFeature
+  ['downloads', 'documentation', 'libraries',
+   'community', 'news', 'security', 'about'].each do |lnk|
+    element(lnk) { |b| b.div(:id, 'header_content').a(href: /\/#{lnk}/) }
+  end
+end
+
+# A page feature. This one models the footer bar with links that runs across the bottom of
+# all of the site's pages. It can be added to a page object by calling the use_features
+# method when defining a page's class. When added to a page class, the initialized page has
+# an accessor method for it (see usage below.)
+class FooterBar < PageFeature
+  ['downloads', 'documentation', 'libraries',
+   'community', 'news', 'security', 'about'].each do |lnk|
+    element(lnk) { |b| b.div(:id, 'footer').a(href: /\/#{lnk}/) }
+  end
+end
+
+# Models the page that users first see when they access the site. The landing page will
+# display summaries of the four most recent news posts. You can click on these summaries to
+# drill down to a page that contains the complete news post. The landing page also has links
+# to navigate to the news page, which has a larger selection of news posts (the last ten
+# most recent posts.)
+class LandingPage < RubyLangSite::Page
+  # Sets a templated URL that will be used for navigation (and for URL matching if a URL
+  # matcher isn't provided.)
+  set_url "/{language}/"
+  use_features :header_bar, :footer_bar # See HeaderBar and FooterBar defined above.
+
+  # Create a method that takes all of the landing page post divs and wrap some more
+  # functionality around them. Also see PostSummary class above.
+  def posts
+    @browser.divs(:class, 'post').map { |div| PostSummary.new(div) }
+  end
+end
+
+# Models the news page, which shows summaries of the last ten most recent posts. The user
+# can drill down on these summaries to read the full story.
+class NewsPage < RubyLangSite::Page
+  # Sets a templated URL that will be used for navigation (and for URL matching if a URL
+  # matcher isn't provided.) See HeaderBar and FooterBar page features defined above.
+  set_url "/{language}/news/"
+  use_features :header_bar, :footer_bar
+
+  # Returns all post summary divs with a little extra functionality wrapped around them.
+  def posts
+    @browser.divs(:class, 'post').map { |div| PostSummary.new(div) }
+  end
+end
+
+# This page hosts a single, complete, news post. Users get to it by drilling down on
+# summaries on the landing page or the news page.
+class NewsPostPage < RubyLangSite::Page
+  set_url_matcher  %r{/en/news/\d+/\d+/\d+/\S+/} #
+  disable_automatic_navigation
+  use_features :header_bar, :footer_bar
+
+  element(:post) { |b| Post.new(b.div(:id, 'content-wrapper')) }
+end
+
+# An element container class. This class adds a little bit of functionality to the
+# underlying element.
+class Post < ElementContainer
+  def post_title
+    links[0]
+  end
+
+  def post_info
+    p(:class, 'post-info')
+  end
+end
+
+# An element container class. This class adds a little bit of functionality to the Post
+# class it inherits from.
+class PostSummary < Post
+
+  def continue_reading
+    links.last
+  end
+end
